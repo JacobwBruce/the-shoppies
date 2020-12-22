c5649492d52ff42172e5fc60935e31542ecc9174
JacobwBruce <jacobwbruce@hotmail.com>
2020-12-22 09:32:12 -0500

    movies state & addMovie, removeMovie functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import MovieInterface from './interfaces/MovieInterface';
+
+//@ts-ignore
+export const MyContext = React.createContext();
 
 function App() {
+    const [movies, setMovies] = useState<Array<MovieInterface>>([]);
+
+    const addMovie = (movie: MovieInterface) => {
+        const newMovies = [...movies];
+
+        newMovies.push(movie);
+
+        setMovies(newMovies);
+    };
+
+    const removeMovie = (id: string) => {
+        let newMovies = [...movies];
+
+        newMovies = newMovies.filter((movie) => movie.imdbID !== id);
+
+        setMovies(newMovies);
+    };
+
     return (
-        <div className='App'>
-            <h1>The Shoppies</h1>
-        </div>
+        <MyContext.Provider value={{ movies, addMovie, removeMovie }}>
+            <div className='App'>
+                <h1>The Shoppies</h1>
+            </div>
+        </MyContext.Provider>
     );
 }
 
