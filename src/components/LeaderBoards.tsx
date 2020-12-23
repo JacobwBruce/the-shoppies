11bd94242bf29c481e494391f3f40e404c719b8f
JacobwBruce <jacobwbruce@hotmail.com>
2020-12-23 08:24:17 -0500

    Set up React Router

---
+++ src/components/LeaderBoards.tsx
@@ -0,0 +1,8 @@
+import './LeaderBoards.css';
+import React, { FC } from 'react';
+
+const LeaderBoards: FC = () => {
+    return <div>LeaderBoards</div>;
+};
+
+export default LeaderBoards;
--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import LeaderBoards from './components/LeaderBoards';
+
+const Routing = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route exact path='/' component={App} />
+                <Route path='/leaderboards' component={LeaderBoards} />
+            </Switch>
+        </Router>
+    );
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Routing />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
